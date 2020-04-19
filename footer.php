1f152956c8c65f0956026cf7ce5bbf3456fc4a11
Naimesh02 <naimeshjariwala02@gmail.com>
2020-04-19 18:53:44 +0530

    firstcommit

---
+++ footer.php
@@ -0,0 +1,39 @@
+<div class="footer">
+	<div class="container-fluid" id="contact-bar">
+    <img src="w3images/Footer-logo.png" class="footer-logo"></img>  
+  		<ul class="list-inline text-center">
+    		<li>
+      			<a href="#"><i class="footer-icon fa fa-facebook"></i></a>
+    		</li>
+
+    		<li>
+      			<a href="#"><i class="footer-icon fa fa-twitter"></i></a>
+    		</li>
+
+    		<li>
+      			<a href="#"><i class="footer-icon fa fa-google"></i></a>
+    		</li>
+
+    		<li>
+      			<a href="#"><i class="footer-icon fa fa-instagram"></i></a>
+    		</li>
+  		</ul>
+	</div>
+	<h6 class="footer-details">copyright © 2017 Droid5 Informatics Pvt Ltd</h6>
+	<h6 class="footer-details-md1">www.droid5.com</h6>
+</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+	
---
+++ header.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+	<title>Andriod Project</title>
+
+	<meta charset="utf-8">
+
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  	<link rel="stylesheet" type="text/css" href="style.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'> -->
+    <link rel="stylesheet" href="category-style.css">
+    
+
+<body>
+
+<div class="social-menu-section">
+	   <a href="#" class="fa fa-facebook"></a>
+	   <a href="#" class="fa fa-twitter"></a>
+	   <a href="#" class="fa fa-google"></a>
+	   <a href="#" class="fa fa-instagram"></a>
+</div>
+
+<div class="navigation-menu">
+<header class="header">
+    <h1 class="logo">
+      <img src="w3images/Header-Logo.png" style="width:40%;"></img>
+    </h1>
+      <ul class="main-nav">
+          <li><a href="#" class="font-color">RXJAVA</a></li>
+          <li><a href="#" class="font-color">BLOGS</a></li>
+          <li><a href="#" class="font-color">STORE</a></li>
+          <li><a href="#" class="font-color">MAGAZINE</a></li>
+          <li><a href="#" class="font-color">TIPS</a></li>
+          <li><a href="#" class="font-color">GITHUB</a></li>
+      </ul>
+  </header> 
+</div>
+
+
