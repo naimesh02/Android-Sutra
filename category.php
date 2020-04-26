c646c918097eb514f6c6a4ce11596bfc1183373e
devyani <panwaladevyani@gmail.com>
2020-04-26 18:58:07 +0530

    Cateogry Page

--- category.php
+++ category.php
@@ -1,15 +1,27 @@
 <?php include 'header.php';?>
-
+<?php include 'conn.php';?>
 <div class="container">
+<?php 
+$qry="SELECT * FROM `tblcategorydata` where category_name='".$_GET['category']."'";
+$cat=mysqli_query($conn,$qry);
+while($row=mysqli_fetch_array($cat))
+{
+?>
+
+
+<div class="row">
 	<div class="row category-row">
 		<div class="col-sm-4">
-			<img src="https://www.androidhive.info/wp-content/uploads/2020/01/banner-370x247.jpeg" class="category-img" alt="Denim Jeans">
+			<img src="../../AdminAndroid/Admin/upload/<?php echo $row['image']; ?>" class="category-img" alt="Denim Jeans">
 		</div>
     	<div class="col-sm-8">
-    		<p class="category-title">Some text about the jeans</p>
-        	<p class="category-desc">Android is a mobile operating system based on a modified version of the Linux kernel and other open source software</p>
+    		<p class="category-title"><?php echo $row['title']; ?></p>
+        	<p class="category-desc"><?php echo $row['description']; ?></p>
     	</div>
+		</div>
 	</div>	
-</div>
 
+<?php 
+}?>
+</div>
 <?php include 'footer.php';?>
