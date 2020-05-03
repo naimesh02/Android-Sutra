f88a94d325e1eb68473f9ffce838cee785556710
Naimesh02 <naimeshjariwala02@gmail.com>
2020-05-03 19:31:16 +0530

    viewcategory

--- category.php
+++ category.php
@@ -1,24 +1,27 @@
 <?php include 'header.php';?>
 <?php include 'conn.php';?>
-
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<div class="w3-container">
 <?php 
 	$qry="SELECT * FROM `tblcategorydata` where category_name='".$_GET['category']."'";
 	$cat=mysqli_query($conn,$qry);
 	while($row=mysqli_fetch_array($cat))
 {
 ?>
-<div class="container">
-	<div class="row category-row">
-		<div class="col-sm-4">
+<div class="w3-panel w3-card">
+	<div class="w3-row">
+		 <div class="w3-third w3-center">
 			<img src="../../Android Project/AdminAndroid/Admin/upload/<?php echo $row['image']; ?>" class="category-img" alt="Denim Jeans">
 		</div>
-    	<div class="col-sm-8">
+		 <div class="w3-third w3-center">
     		<p class="category-title"><?php echo $row['title']; ?></p>
         	<p class="category-desc"><?php echo $row['description']; ?></p>
+    	</p>
     	</div>
-		</div>
-	</div>	
+    </div>
+</div>	
 
 <?php 
 }?>
+</div>
 <?php include 'footer.php';?>
---
+++ view-category.php
@@ -0,0 +1,26 @@
+<?php include 'header.php' ?>
+
+<div class='container'>
+	<div id='content' class='row-fluid'>
+		<div class='span2 sidebar'>
+			<img src="Images\Fotojet.jpg"></img>
+		</div>
+
+		<div class='span8 main'>
+			<img src="https://s3.ap-south-1.amazonaws.com/cms-abp-prod-media/library/THE_TELEGRAPH/mig/1120223/images/apps.jpg" class="center"></img>
+
+			<h2>Application</h2>
+
+			<p class="category-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+
+			<iframe src="http://www.youtube.com/embed/W7qWa52k-nE" style="padding:20px;" width="570" height="335" frameborder="0" allowfullscreen></iframe>
+
+		</div>
+
+		<div class='span2 sidebar'>
+			<img src="Images\Fotojet.jpg"></img>
+		</div>
+	</div>
+</div>
+<br><br>
+<?php include 'footer.php' ?>
