af2959f7230b0ee269477e79251a0ca7bea5d427
devyani <panwaladevyani@gmail.com>
2020-05-10 14:01:56 +0530

    pagination

--- Data.php
+++ Data.php
@@ -1,10 +1,29 @@
 <?php include 'header.php';?>
 <?php include 'conn.php';?>
-
+<link rel="stylesheet" type="text/css" href="css/Index-Style.css">
 
 <?php 
+$results_per_page=2;
 $qry="SELECT * FROM `tbldata` where type='".$_GET['type']."'";
 $cat=mysqli_query($conn,$qry);
+$num_of_data=mysqli_num_rows($cat);
+
+$num_of_pages=ceil($num_of_data/$results_per_page);
+
+if(!isset($_GET['page'])){
+	$page=1;
+}
+else
+{
+	$page=$_GET['page'];
+}
+
+$this_page_start=($page-1)*$results_per_page;
+
+
+$qry="SELECT * FROM `tbldata` where type='".$_GET['type']."' LIMIT ".$this_page_start.','.$results_per_page;
+$cat=mysqli_query($conn,$qry);
+
 while($row=mysqli_fetch_array($cat))
 {
 ?>
@@ -20,6 +39,20 @@ while($row=mysqli_fetch_array($cat))
 	</div>
 </div>	
 <?php 
-}?>
+}
+?>
+<div class="container pagination">
+	<div class="row">
+		<div class="col-sm-6"></div>
+		<div class="col-sm-6">
+<?php
+for($i=1;$i<=$num_of_pages;$i++){
+	echo '<a href="Data.php?type='.$_GET['type'].'&page='.$i.'" style="font-size:17px;padding:5px;color:black">'.$i.'</a>';
+}
+?>
+</div>
+</div>
+</div>
+
 
 <?php include 'footer.php';?>
--- header.php
+++ header.php
@@ -1,33 +1,34 @@
 <html>
 <head>
-	<title>Andriod Project</title>
+  <title>Andriod Project</title>
 
-	<meta charset="utf-8">
+  <meta charset="utf-8">
 
-  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-  	<link rel="stylesheet" type="text/css" href="css/Index-Style.css">
+    <link rel="stylesheet" type="text/css" href="css/Index-Style.css">
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
     <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'> -->
     <link rel="stylesheet" href="category-style.css">
-<!--	<script src="js/stickyHeader.js"></script>-->
+<!--  <script src="js/stickyHeader.js"></script>-->
 
 <!-- SiderBar CSS -->
 <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
 <body>
 
 <div class="social-menu-section">
-	   <a href="#" class="fa fa-facebook"></a>
-	   <a href="#" class="fa fa-twitter"></a>
-	   <a href="#" class="fa fa-google"></a>
-	   <a href="#" class="fa fa-instagram"></a>
+     <a href="#" class="fa fa-facebook"></a>
+     <a href="#" class="fa fa-twitter"></a>
+     <a href="#" class="fa fa-google"></a>
+     <a href="#" class="fa fa-instagram"></a>
 </div>
 
 <div class="navigation-menu">
@@ -58,12 +59,13 @@
 
 
 <script>
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction1()};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
-function myFunction() {
+function myFunction1() {
+  
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
