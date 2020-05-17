0680cfa2a26b3ca8d2f091a89c35fda29925e0e1
Naimesh02 <naimeshjariwala02@gmail.com>
2020-05-17 13:16:05 +0530

    Changes

--- category.php
+++ category.php
@@ -2,6 +2,17 @@
 <?php include 'conn.php';?>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <div class="w3-container">
+	 <div class="container_title">
+    <div class="decorative-title">
+      <div class="decorative-bar left vertical thick"></div>
+      <div class="decorative-bar left horizontal thick"></div>
+      <div class="decorative-bar left horizontal thin"></div>
+      <span>DATA OF CATEGORY <?php echo strtoupper($_GET['category']);?></span>
+      <div class="decorative-bar right vertical thick"></div>
+      <div class="decorative-bar right horizontal thick"></div>
+      <div class="decorative-bar right horizontal thin"></div>
+    </div>
+  </div>
 <?php 
 	$qry="SELECT * FROM `tblcategorydata` where category_name='".$_GET['category']."'";
 	$cat=mysqli_query($conn,$qry);
@@ -12,7 +23,7 @@
 	<div class="w3-row">
 		 <div class="w3-third w3-center">
 		 	<div class="w3-panel w3-card w3-card-view">
-			<img src="../../AdminAndroid/Admin/upload/<?php echo $row['image']; ?>" class="category-img" alt="
+			<img src="../../Android Project/AdminAndroid/Admin/upload/<?php echo $row['image']; ?>" class="category-img" alt="
 			Denim Jeans">
 		</div>
 		</div>
